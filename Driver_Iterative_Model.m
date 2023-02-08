541ee03c5a798714448aa36bce9c0e92a6324bcf
jcale <jcale@thegreenhornet.lan>
2023-02-08 01:00:13 -0500

    Continued development of Iterative Model driver

--- Driver_Iterative_Model.m
+++ Driver_Iterative_Model.m
@@ -10,22 +10,29 @@ R_e = 6378.14e3;
 km = 1e3;
 
 %% Set input
+
+% TLE
+TLE = "25544 ISS (ZARYA).txt";
+t = [0:60:48*hr]; %MUST BE GREATER THAN 1 PERIOD
+
+% measurment
+t_1 = .3;
+t_2 = .5;
+
+
+%% Propogate orbit
+
+[OE, x_h] = createOrbit(TLE, t);
+
+% standard TBP check
 OE = OEfromTLE("25544 ISS (ZARYA).txt")
 [r_0, v_0] = OE2rv(OE);
-
-% r_0 = [-4580.3; 6582.7; -180.95]*km; %m
-% v_0 = [-5.8396; 3.0075; 1.758]*km; %m/s
-
 initCond = [r_0; v_0];
-
-
-%% Propogate forward
-t = [0:60:48*hr];
 options = odeset('AbsTol',1e-9,'RelTol',1e-6);
 [~,x] = ode45('TBP', t, initCond, options);
-[t_h,x_h] = ode45('TBP_Harmonics', t, initCond, options);
-
 r = x(:,1:3);
+
+[t_h,x_h] = ode45('TBP_Harmonics', t, initCond, options);
 r_h = x_h(:,1:3)
 
 figure()
@@ -33,3 +40,7 @@ grid on
 hold on
 plot3(r(:,1),r(:,2),r(:,3))
 plot3(r_h(:,1),r_h(:,2),r_h(:,3))
+hold off
+
+%% Create measurment
+
