c0631f63e249f43e852b54401201b25b9e0c5989
Joey Caley <Caley.11@buckeyemail.osu.edu>
2023-02-20 12:53:29 -0500

    Added in a loop and some histories for different variables. Basically, J2 is recalculated after each propogation which starts from 0. All msmts and histories are now 2 dimensional to account for loops and different measurements.      All the mods in iterative kepler are left over from my meeting with Kumar last week, just some input changes and messing with which controller I was running while showing him stuff.

--- Driver_Iterative_Model.m
+++ Driver_Iterative_Model.m
@@ -19,10 +19,10 @@ TLE = "25544 ISS (ZARYA).txt";
 t = [0:60:48*hr]; %MUST BE GREATER THAN 1 PERIOD
 
 % measurment time as function of period
-t_m = [.3 .5];
+t_msmt = [.3 .5];
 
 % gains
-G = 0.010;
+Kp = 0.010;
 
 % initial guess
 J2est = 1*10^-6;
@@ -35,6 +35,7 @@ maxiter = 500;
 % harmonics dynamics
 [OE, x_h, initCond] = createOrbit(TLE, t);
 r_orbit = x_h(:,1:3);
+v_orbit = x_h(:,4:6);
 
 % standard TBP check
 OE = OEfromTLE("25544 ISS (ZARYA).txt");
@@ -57,26 +58,35 @@ hold off
 % convert to times
 a = OE(1);
 P = 2*pi*sqrt(a^3/mu_e);
-t_m = t_m*P;
+t_msmt = t_msmt*P;
 
-for i = 1:length(t_m)
-    [r_m(i), r_dm(i)] = create_msmt(1, t_m(i), P, r_orbit, t);
+for i = 1:length(t_msmt)
+    [r_msmt(i), dr_msmt(i)] = create_msmt(1, t_msmt(i), P, r_orbit, t);
 end
 
 %% Setup first run
 
 % create histories
-J2est_hist = zeros(maxiter,1);
-r2est_hist = zeros(maxiter,1);
-
-
-options = odeset('AbsTol',1e-9,'RelTol',1e-6);
-[~,x] = ode45('TBP', t, initCond, options);
+rest_hist = zeros(maxiter, length(t_msmt));
+err_hist = zeros(maxiter, length(t_msmt));
+J2est_hist = zeros(maxiter, length(t_msmt));
 
 %% Iterations
 
-for i = 2:maxiter
-    
-    
+for i = 1:maxiter
+    for j = 1:length(t_msmt)
+        % Propogate from 0
+        options = odeset('AbsTol',1e-9,'RelTol',1e-6);
+        [t_loop,x_loop] = ode45(@(t_loop,x_loop) TBP_UnknownHarmonics(...
+            t_loop,x_loop,J2est), [0 t_msmt(j)], initCond, options);
+        r_est = norm(x_loop(end,1:3));
 
+        err = r_est - r_msmt(j);
+        J2est = J2est - Kp*err;
+
+        % input histories
+        err_hist(i,j) = err;
+        rest_hist(i,j) = r_est;
+        J2est_hist(i,j) = J2est;
+    end
 end
--- functions/TBP_UnknownHarmonics.m
+++ functions/TBP_UnknownHarmonics.m
@@ -1,4 +1,4 @@
-function [dots] = TBP_UnknownHarmonics(~,initial_conditions,J_2)
+function [dots] = TBP_UnknownHarmonics(t,initial_conditions,J_2)
 
 % Grabbing the initial conditions 
 r1 = initial_conditions(1);
@@ -14,7 +14,7 @@ r = norm([r1 r2 r3]);
 mu = 3.986004*10^14; %Assume Earth centric
 r_eq = 6378.137*10^3;
 denominator = (r1^2 + r2^2 + r3^2)^(3/2);
-J_2 = 1082.63*10^-6;
+% J_2 = 1082.63*10^-6;
 
 a_J2 = -3/2*J_2*(mu/r^2)*(r_eq/r)^2 * [...
     (1 - 5*(r3/r)^2) * r1/r; ...
